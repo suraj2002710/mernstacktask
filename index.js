2546b87cc9847bb5b62c01cb3e0029aa284f572d
suraj2002710 <113814706+suraj2002710@users.noreply.github.com>
2023-09-01 08:54:15 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,12 +2,15 @@ const express=require('express')
 const app=express()
 const mongoose=require('mongoose')
 const router = require('./routes/routes')
-
+const cors=require("cors")
 mongoose.connect('mongodb+srv://surajaheer2002:suraj12345@cluster0.hcq5sxu.mongodb.net/chat?retryWrites=true&w=majority').then((host)=>{
     console.log("mongo connect")
 }).catch((err)=>{
     console.log(err)
 })
+app.use(cors({
+    origin:"*"
+}))
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.use('/',router)
